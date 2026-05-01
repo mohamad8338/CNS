ec59f3eda75aa41a79aa1d8fd003320416a7acc7
SofiaPetronelle <not.aradak@gmail.com>
2026-05-01 03:31:14 +0330

    chore(main): drop startup console noise

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,11 @@ import App from './App'
 import { logger } from './lib/logger'
 import './index.css'
 
-console.log('[CNS] Application starting...')
 logger.init()
 logger.info('App startup initiated')
 
 const root = document.getElementById('root')
 if (!root) {
-  console.error('[CNS] Fatal: root element not found')
   logger.error('Root element not found')
 } else {
   logger.info('Root element found, mounting React')
